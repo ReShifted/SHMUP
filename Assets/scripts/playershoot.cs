b42f0ff38f6bed43f3faba0b768e83c46fcbb084
ReShifted <144238154+ReShifted@users.noreply.github.com>
2026-03-24 14:11:43 +0100

    spreadshot

--- Assets/scripts/playershoot.cs
+++ Assets/scripts/playershoot.cs
@@ -7,6 +7,9 @@ public class playershoot : MonoBehaviour
     public GameObject parryTHISSHIT;
     private float shieldlastFireTime = -2f;
     public float shieldfireCooldown = 2f;
+    public float spreadshotcooldown = 2f;
+
+    private float[] angles = {15f,0f,-15f};
 
     void Start()
     {
@@ -15,14 +18,28 @@ public class playershoot : MonoBehaviour
 
     void Update()
     {
+        // Shoot with space key
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(BulletPrefab, transform.position, Quaternion.identity);
         }
+
+        // parry with E key
         if (Input.GetKeyDown(KeyCode.E)&& Time.time >= shieldlastFireTime + shieldfireCooldown)
         {
             shieldlastFireTime = Time.time;
             Instantiate(parryTHISSHIT, transform.position, Quaternion.identity);
+        }
+
+        // Spread shot with Q key
+        if (Input.GetKeyDown(KeyCode.Q) && Time.time >= shieldlastFireTime + spreadshotcooldown)
+        {
+            shieldlastFireTime = Time.time;
+            foreach (float angle in angles)
+            {
+                Quaternion rotation = Quaternion.Euler(0, 0, angle);
+                Instantiate(BulletPrefab, transform.position, rotation);
+            }
         }
     }
 }
