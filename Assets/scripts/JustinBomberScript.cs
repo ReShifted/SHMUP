5965d99ce985f7fc2386c43636f2ee1489489c0d
Robkop-glu <robinvanderbie@gmail.com>
2026-03-25 12:01:22 +0100

    !fix not full target

--- Assets/scripts/JustinBomberScript.cs
+++ Assets/scripts/JustinBomberScript.cs
@@ -3,31 +3,53 @@ using UnityEngine;
 public class JustinBomberScript : MonoBehaviour
 {
     public float speed = 5f;
-    [SerializeField] Transform Player2;
+    [SerializeField] private Transform player2;
+    [Range(0f, 1f)] public float homingStrength = 0.3f;
 
-    void Update()
+    private Rigidbody rb;
+
+    void Awake()
     {
-        MoveLeft();
-        FollowPlayerY();
+        rb = GetComponent<Rigidbody>();
     }
 
-    void MoveLeft()
+    void FixedUpdate()
     {
-        transform.position += Vector3.left * speed * Time.deltaTime;
-    }
+        if (rb != null)
+        {
+            float targetY = transform.position.y;
 
-    void FollowPlayerY()
-    {
-        if (Player2 == null) return;
+            if (player2 != null)
+                targetY = player2.position.y;
 
-        Vector3 target = transform.position;
-        target.y = Player2.position.y;
+            float homingSpeed = speed * homingStrength;
+            float newY = Mathf.MoveTowards(
+                transform.position.y,
+                targetY,
+                homingSpeed * Time.fixedDeltaTime
+            );
 
-        transform.position = Vector3.MoveTowards(
-            transform.position,
-            target,
-            speed * Time.deltaTime
-        );
+            float vy = (newY - transform.position.y) / Time.fixedDeltaTime;
+
+            rb.linearVelocity = new Vector3(-speed, vy, 0f);
+        }
+        else
+        {
+            Vector3 pos = transform.position;
+            pos += Vector3.left * speed * Time.fixedDeltaTime;
+
+            if (player2 != null)
+            {
+                float homingSpeed = speed * homingStrength;
+                pos.y = Mathf.MoveTowards(
+                    pos.y,
+                    player2.position.y,
+                    homingSpeed * Time.fixedDeltaTime
+                );
+            }
+
+            transform.position = pos;
+        }
     }
 
     void OnCollisionEnter(Collision collision)
