fcfe520cab7025101a4064c55a15ee01e50392ff
ReShifted <144238154+ReShifted@users.noreply.github.com>
2026-03-25 10:25:07 +0100

    Revert "robin's fuel idk"
    This reverts commit 4726c21b96f170f51f2b05a576464d9181b77412, reversing
    changes made to b322a0d34ababf4860b1b13834d30828d9fca6a1.

--- Assets/scripts/playerHP.cs
+++ Assets/scripts/playerHP.cs
@@ -1,25 +1,22 @@
-using JetBrains.Annotations;
-using System.Runtime.Serialization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class playerHP : MonoBehaviour
 {
     public float maxHP = 100;
-    public float currentHP = 100;
+    public float currentHP=100;
     public HealthCounterPLACEHOLDER HEalthCounter;
-
     void Update()
     {
         maxHP = currentHP;
     }
 
     void Start()
-    {
-        //currentHP = maxHP;
+{
+    //currentHP = maxHP;
 
-        HEalthCounter.healthCounter(currentHP);
-    }
+    HEalthCounter.healthCounter(currentHP);
+}
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("EnemyBullet"))
@@ -29,8 +26,28 @@ public class playerHP : MonoBehaviour
             {
                 Debug.Log("Player has died.");
                 SceneManager.LoadScene("restartscreen");
-                //  die();
+              //  die();
             }
         }
     }
+
+    //public void TakeDamage(int amount)
+    //{
+    //    currentHP -= amount;
+
+    //    if (currentHP <= 0)
+    //    {
+    //        SceneManager.LoadScene("restartscreen");
+    //        die();
+    //    }
+
+    //    // Update UI
+    //    HealthCounterPLACEHOLDER.instance.healthCounter(currentHP);
+    //}
+
+    //public void die()
+    //{
+    //    Debug.Log("Player has died.");
+    //    Destroy(gameObject);
+    //}
 }
