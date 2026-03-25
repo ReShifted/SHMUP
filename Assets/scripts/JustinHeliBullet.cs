02c2c9a3cdd0360da24856a65340b9221a308daf
ReShifted <144238154+ReShifted@users.noreply.github.com>
2026-03-25 11:27:15 +0100

    parry added to new bullets

--- Assets/scripts/JustinHeliBullet.cs
+++ Assets/scripts/JustinHeliBullet.cs
@@ -2,13 +2,37 @@ using UnityEngine;
 
 public class JustinHeliBullet : MonoBehaviour
 {
+    public TimeManager Timemanager;
+    private Rigidbody rb;
+
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        Timemanager = FindFirstObjectByType<TimeManager>();
+
         Destroy(gameObject, 2f);
+    }
+
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("PARRYIT") && this.CompareTag("EnemyBullet"))
+        {
+            this.tag = "PlayerBullet";
+            CancelInvoke(nameof(DestroySelf));
+            Invoke(nameof(DestroySelf), 4f);
+            rb.linearVelocity = -rb.linearVelocity * 2f;
+            Timemanager.DoSlowmotion();
+        }
+        else if (other.CompareTag("PARRYIT") && this.CompareTag("PlayerBullet"))
+        {
+            rb.linearVelocity = rb.linearVelocity * 2f;
+        }
+
         if (other.CompareTag("Player"))
         {
             Destroy(gameObject);
--- Assets/scripts/justinEyeBullet.cs
+++ Assets/scripts/justinEyeBullet.cs
@@ -2,14 +2,49 @@ using UnityEngine;
 
 public class justinEyeBullet : MonoBehaviour
 {
+    public TimeManager Timemanager;
+    private Rigidbody rb;
+    private float speed = 8f;
+
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        Timemanager = FindFirstObjectByType<TimeManager>();
+
+        rb.linearVelocity = new Vector3(-speed, 0, 0);
+
         Destroy(gameObject, 2f);
+    }
+
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("PARRYIT") && this.CompareTag("EnemyBullet"))
+        {
+            this.tag = "PlayerBullet";
+            CancelInvoke(nameof(DestroySelf));
+            Invoke(nameof(DestroySelf), 4f);
+            rb.linearVelocity = -rb.linearVelocity * 2f;
+            Timemanager.DoSlowmotion();
+        }
+        else if (other.CompareTag("PARRYIT") && this.CompareTag("PlayerBullet"))
+        {
+            rb.linearVelocity = rb.linearVelocity * 2f;
+        }
+
         if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject);
         }
