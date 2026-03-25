1c2ad267624fa0b65fae8525249639459efb5ae3
Robkop-glu <robinvanderbie@gmail.com>
2026-03-25 09:30:42 +0100

    !feul

--- Assets/scripts/playerHP.cs
+++ Assets/scripts/playerHP.cs
@@ -1,22 +1,28 @@
+using JetBrains.Annotations;
+using System.Runtime.Serialization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class playerHP : MonoBehaviour
 {
     public float maxHP = 100;
-    public float currentHP=100;
+    public float currentHP = 100;
     public HealthCounterPLACEHOLDER HEalthCounter;
+    public float feulAmount = 100f;
+    public float TimeForFeulDown = 1f;
+
     void Update()
     {
         maxHP = currentHP;
+        feul();
     }
 
     void Start()
-{
-    //currentHP = maxHP;
+    {
+        //currentHP = maxHP;
 
-    HEalthCounter.healthCounter(currentHP);
-}
+        HEalthCounter.healthCounter(currentHP);
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("EnemyBullet"))
@@ -26,28 +32,41 @@ public class playerHP : MonoBehaviour
             {
                 Debug.Log("Player has died.");
                 SceneManager.LoadScene("restartscreen");
-              //  die();
+                //  die();
             }
         }
     }
+    public void feul()
+    {
+        if (feulAmount > 0)
+        {
+            feulAmount -= Time.deltaTime * (100 / TimeForFeulDown);
+        }
+        else if (feulAmount <= 0)
+        {
+            {
+                SceneManager.LoadScene("restartscreen");
+            }
+        }
 
-    //public void TakeDamage(int amount)
-    //{
-    //    currentHP -= amount;
+        //public void TakeDamage(int amount)
+        //{
+        //    currentHP -= amount;
 
-    //    if (currentHP <= 0)
-    //    {
-    //        SceneManager.LoadScene("restartscreen");
-    //        die();
-    //    }
+        //    if (currentHP <= 0)
+        //    {
+        //        SceneManager.LoadScene("restartscreen");
+        //        die();
+        //    }
 
-    //    // Update UI
-    //    HealthCounterPLACEHOLDER.instance.healthCounter(currentHP);
-    //}
+        //    // Update UI
+        //    HealthCounterPLACEHOLDER.instance.healthCounter(currentHP);
+        //}
 
-    //public void die()
-    //{
-    //    Debug.Log("Player has died.");
-    //    Destroy(gameObject);
-    //}
+        //public void die()
+        //{
+        //    Debug.Log("Player has died.");
+        //    Destroy(gameObject);
+        //}
+    }
 }
