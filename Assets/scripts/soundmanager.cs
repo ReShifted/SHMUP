777ce3e837b861037671a9246fe77c32b3905aed
theamazingrat <dinandvdvecht@gmail.com>
2026-04-02 14:58:24 +0200

    Revert "Update soundmanager.cs"
    This reverts commit ce2618bcd4fcb7f66136f87e75568d8159bee2ac.

--- Assets/scripts/soundmanager.cs
+++ Assets/scripts/soundmanager.cs
@@ -4,6 +4,7 @@ using UnityEngine.Audio;
 
 public class soundmanager : MonoBehaviour
 {
+    private AudioSource audiosource;
 
     [SerializeField] public AudioSource parrySound;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -14,32 +15,23 @@ public class soundmanager : MonoBehaviour
         {
             instance = this;
         }
-<<<<<<< HEAD
         else if(instance != this)
         {
             Destroy(gameObject);
         }
 
         //DontDestroyOnLoad(gameObject);
-=======
-        
->>>>>>> parent of 259d473 (!fix parry boss)
     }
     public void PlayParrySound(AudioClip audioclip, Transform spawntransform)
     {
-<<<<<<< HEAD
         if (audiosource == null || !audiosource.isPlaying)
-=======
-        if (!parrySound.isPlaying)
->>>>>>> parent of 259d473 (!fix parry boss)
         {
             // AudioSource.PlayClipAtPoint(PARRY, Camera.main.transform.position);
-            AudioSource audiosource = Instantiate(parrySound, spawntransform.position, Quaternion.identity);
+            audiosource = Instantiate(parrySound, spawntransform.position, Quaternion.identity);
             audiosource.clip = audioclip;
             audiosource.Play();
             float cliplength = audioclip.length;
             Destroy(audiosource.gameObject, cliplength);
         }
-
     }
 }
